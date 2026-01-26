720a43afb115a939ea93aedf8e1cc9c6cc4c38ff
@notunrandom <>
2026-01-26 15:41:08 +0000

    Create test of lazyness of BL.pack/unpack

--- tdd/LazyByteString.hs
+++ tdd/LazyByteString.hs
@@ -2,10 +2,14 @@
 
 module LazyByteString where
 
+import qualified Data.ByteString.Lazy as B
+import Data.Word (Word8)
 import Test.QuickCheck
 
 prop_test :: Property
-prop_test = once $ (1::Int) === (1::Int)
+prop_test =
+  once $
+      (1::Word8) == (head $ B.unpack $ B.pack $ repeat 1)
 
 
 -----------DO NOT WRITE BELOW THIS LINE (needed for TemplateHaskell)-----------
