83e9cf263bd2cfbcac7197f5693e324b84ec1d59
@notunrandom <>
2026-01-26 12:54:20 +0000

    Try Serialise tutorial

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,8 +1,42 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
-import qualified MyLib (someFunc)
+import Codec.Serialise
+import qualified Data.ByteString.Lazy as BSL
+import GHC.Generics (Generic)
+import Prelude hiding (readIO)
+
+fileName :: FilePath
+fileName = "out.cbor"
+
+data Animal = HoppingAnimal { animalName :: String, hoppingHeight :: Int }
+            | WalkingAnimal { animalName :: String, walkingSpeed  :: Int }
+            deriving (Generic,Show)
+
+instance Serialise Animal
+
+fredTheFrog :: Animal
+fredTheFrog = HoppingAnimal "Fred" 4
+
+-- | To output value into a file
+write :: Serialise a => FilePath -> a -> IO ()
+write file val = BSL.writeFile file (serialise val)
+
+-- | Outputs @Fred@ value into file
+writeIO :: IO ()
+writeIO = write fileName fredTheFrog
+
+-- | Reads the value from file
+readIO :: IO Animal
+readIO = deserialise <$> BSL.readFile fileName
+
+printIO :: IO ()
+printIO = do
+    val <- readIO
+    print val
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  writeIO
+  printIO
